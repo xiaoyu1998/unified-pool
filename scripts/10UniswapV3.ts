bd8a5cd69b5fd44c58a54fc26775cb0134c8cc67
xiaoyu1998 <1634054493@qq.com>
2024-04-12 13:31:28 +0800

    add price decode and encode with uniswap Sqrt96

--- scripts/10UniswapV3.ts
+++ scripts/10UniswapV3.ts
@@ -1,5 +1,5 @@
 import { contractAt, sendTxn, getDeployedContractAddresses, getTokens, getContract, getContractAt } from "../utils/deploy";
-import { expandDecimals } from "../utils/math";
+import { expandDecimals, encodePriceSqrt, decodePriceSqrt } from "../utils/math";
 import { getPoolInfo, getLiquidity, getDebt} from "../utils/helper";
 import { FeeAmount} from "../utils/constants";
 import {
@@ -7,12 +7,19 @@ import {
   bytecode as FACTORY_BYTECODE,
 } from '@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json'
 
+import {
+  abi as POOL_ABI,
+  bytecode as POOL_BYTECODE,
+} from '@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json'
+
 
 const { mine } = require("@nomicfoundation/hardhat-network-helpers");
 
 async function main() {
     const [owner] = await ethers.getSigners();
 
+    const usdtDecimals = 6;
+    const uniDecimals = 18;
     const usdtAddress = getTokens("USDT")["address"];
     const uniAddress = getTokens("UNI")["address"];
 
@@ -20,7 +27,24 @@ async function main() {
     const factory = await contractFactory.deploy();
     await factory.createPool(usdtAddress, uniAddress, FeeAmount.MEDIUM);
 
-    console.log(await factory.getPool(usdtAddress, uniAddress, FeeAmount.MEDIUM));
+    const poolAddress = await factory.getPool(uniAddress, usdtAddress,  FeeAmount.MEDIUM);
+    const contractFactoryPool = new ethers.ContractFactory(POOL_ABI, POOL_BYTECODE, owner);
+    const pool = await contractFactoryPool.attach(poolAddress);
+
+    const sqrtPriceX96 = encodePriceSqrt(expandDecimals(1000, uniDecimals), expandDecimals(10000, usdtDecimals));
+    const price = decodePriceSqrt(sqrtPriceX96);
+    console.log(sqrtPriceX96, price);
+
+    // pool.on("Initialize", (sqrtPriceX96, tick) =>{
+    //     console.log("Initialize" , sqrtPriceX96, tick);
+    // });
+    await pool.initialize(sqrtPriceX96);
+
+    console.log("factory", await pool.factory());
+    console.log("token0", await pool.token0());
+    console.log("token1", await pool.token1());
+    console.log("fee", await pool.fee());
+    console.log("tickSpacing", await pool.tickSpacing());
 
 }
 
--- utils/math.ts
+++ utils/math.ts
@@ -1,3 +1,4 @@
+import * as Math from 'mathjs'
 
 export function bigNumberify(n) {
   // return ethers.toBigInt(n);
@@ -6,4 +7,16 @@ export function bigNumberify(n) {
 
 export function expandDecimals(n, decimals) {
   return bigNumberify(n)*(bigNumberify(10)**ethers.toBigInt(decimals));
+}
+
+// returns the sqrt price as a 64x96
+export function encodePriceSqrt(amount1: BigInt, amount0: BigInt): BigInt {
+  const numerator = amount1 << BigInt(192);
+  const denominator = amount0;
+  const ratioX192 = numerator / denominator;
+  return BigInt(Math.sqrt(ratioX192.toString()));
+}
+
+export function decodePriceSqrt(sqrtPriceX96: BigInt): BigInt {
+   return (sqrtPriceX96 ** BigInt(2)) >> BigInt(192);
 }
