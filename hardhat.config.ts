a9c54447d6cc5070b9da0731a68ce94dd872012e
xiaoyu1998 <1634054493@qq.com>
2024-05-06 18:47:23 +0800

    add localNetwork

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition-ethers";
-// import { ethers } from "ethers";
-// import "@nomicfoundation/hardhat-ethers";
 import { ethers, ignition } from "hardhat";
+
+const {
+  LOCALNETWORK_URL,
+  LOCALNETWORKT_DEPLOY_KEY
+} = require("./env.json")
+
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -19,6 +23,21 @@ const config: HardhatUserConfig = {
          runs: 200
        }
      }
+  },
+  networks:{
+    localNetwork: {
+      url: LOCALNETWORK_URL,
+      chainId: 100,
+      accounts: [LOCALNETWORKT_DEPLOY_KEY],
+      verify: {
+        etherscan: {
+          apiUrl: "https://api-testnet.snowtrace.io/",
+          apiKey: process.env.SNOWTRACE_API_KEY,
+        },
+      },
+      blockGasLimit: 20_000_000,
+      gas: 20_000_000,
+    },
   }
 };
 
