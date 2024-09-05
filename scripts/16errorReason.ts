08c071c79c90ca7dae62654cf61643f2afbcfbc3
xiaoyu1998 <1634054493@qq.com>
2024-09-05 15:56:27 +0800

    update repay error msg

--- scripts/16errorReason.ts
+++ scripts/16errorReason.ts
@@ -5,7 +5,7 @@ async function main() {
   // const txHash = "0xa58cc82917012689706a58b0f7e2716c5022b65df91d9c98232431717edcb53f";
   // console.log("Error:", await getErrorMsgFromTx(txHash));
 
-  const errorBytes = '0x018439d6';
+  const errorBytes = '0x86c4ec210000000000000000000000000000000000000000000000008ac7230489e800000000000000000000000000000000000000000000000000000000000000000000';
   console.log("Error:", await getErrorMsg(errorBytes));
 }
 
